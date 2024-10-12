ed26c566f35c91077a69064a90b285ff9697d064
kurtekat <142125482+kurtekat@users.noreply.github.com>
2024-10-11 22:04:54 -0400

    Update Win32.cs

--- Updater/Common/Win32.cs
+++ Updater/Common/Win32.cs
@@ -8,10 +8,10 @@ namespace Updater.Common
         public static extern IntPtr FindWindowW(string? className, string? windowName);
 
         [DllImport("kernel32", EntryPoint = "GetPrivateProfileIntW", CharSet = CharSet.Unicode)]
-        public static extern uint GetPrivateProfileIntW(string section, string key, int _default, string fileName);
+        public static extern uint GetPrivateProfileIntW(string section, string key, int defaultValue, string fileName);
 
         [DllImport("kernel32", EntryPoint = "GetPrivateProfileStringW", CharSet = CharSet.Unicode)]
-        public static extern uint GetPrivateProfileStringW(string? section, string? key, string? _default, char[] output, uint size, string fileName);
+        public static extern uint GetPrivateProfileStringW(string? section, string? key, string? defaultValue, char[] buffer, uint bufferSize, string fileName);
 
         [DllImport("kernel32", EntryPoint = "WritePrivateProfileStringW", CharSet = CharSet.Unicode)]
         public static extern int WritePrivateProfileStringW(string section, string? key, string? value, string fileName);
