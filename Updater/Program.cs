d74af8025c295290979c9b8387bc32a4f7fbdaa7
kurtekat <142125482+kurtekat@users.noreply.github.com>
2024-10-12 17:07:03 -0400

    Update Program.cs
    Update Program.cs

--- Updater/Program.cs
+++ Updater/Program.cs
@@ -12,72 +12,71 @@ namespace Updater
     {
         public static void DoWork(HttpClient httpClient, BackgroundWorker worker)
         {
-            var serverCfg = new ServerConfiguration(httpClient);
-            var clientCfg = new ClientConfiguration();
-            
-            if (serverCfg.UpdaterVersion > Constants.UpdaterVersion)
+            try
             {
-                UpdaterPatcher(httpClient, worker);
-                return;
-            }
+                var serverCfg = new ServerConfiguration(httpClient);
+                var clientCfg = new ClientConfiguration();
 
-            if (serverCfg.PatchFileVersion > clientCfg.CurrentVersion)
-            {
-                uint progressMax = serverCfg.PatchFileVersion - clientCfg.CurrentVersion;
-                uint progressValue = 1;
-
-                while (clientCfg.CurrentVersion < serverCfg.PatchFileVersion)
+                if (serverCfg.PatchFileVersion > clientCfg.CurrentVersion)
                 {
-                    var version = clientCfg.CurrentVersion + 1;
-                    var patch = new Patch(version);
+                    uint progressMax = serverCfg.PatchFileVersion - clientCfg.CurrentVersion;
+                    uint progressValue = 1;
 
-                    var progressMessage = string.Format(Constants.ProgressMessage1, progressValue, progressMax);
-                    worker.ReportProgress(0, new ProgressReport(progressMessage, 0));
-
-                    Util.DownloadToFile(httpClient, patch.Url, patch.Path);
-
-                    if (!File.Exists(patch.Path))
+                    while (clientCfg.CurrentVersion < serverCfg.PatchFileVersion)
                     {
-                        worker.ReportProgress(0, new ProgressReport(Constants.ProgressMessage2, 0));
-                        break;
-                    }
+                        var version = clientCfg.CurrentVersion + 1;
+                        var patch = new Patch(version);
 
-                    worker.ReportProgress(0, new ProgressReport(Constants.ProgressMessage3, 0));
-                    clientCfg.Write("StartUpdate", "EXTRACT_START");
+                        var progressMessage = string.Format(Constants.ProgressMessage1, progressValue, progressMax);
+                        worker.ReportProgress(0, new ProgressReport(progressMessage, 0));
 
-                    if (Util.ExtractZipFile(patch.Path) != 0)
-                    {
-                        worker.ReportProgress(0, new ProgressReport(Constants.ProgressMessage4, 0));
-                        break;
-                    }
+                        Util.DownloadToFile(httpClient, patch.Url, patch.Path);
 
-                    clientCfg.Write("StartUpdate", "EXTRACT_END");
-                    File.Delete(patch.Path);
+                        if (!File.Exists(patch.Path))
+                        {
+                            worker.ReportProgress(0, new ProgressReport(Constants.ProgressMessage2, 0));
+                            break;
+                        }
 
-                    worker.ReportProgress(0, new ProgressReport(Constants.ProgressMessage5, 0));
-                    clientCfg.Write("StartUpdate", "UPDATE_START");
+                        worker.ReportProgress(0, new ProgressReport(Constants.ProgressMessage3, 0));
+                        Win32.WritePrivateProfileStringW("Version", "StartUpdate", "EXTRACT_START", clientCfg.Path);
 
-                    if (DataPatcher(worker) != 0)
-                    {
-                        worker.ReportProgress(0, new ProgressReport(Constants.ProgressMessage6, 0));
-                        break;
-                    }
+                        if (Util.ExtractZipFile(patch.Path, Directory.GetCurrentDirectory()) != 0)
+                        {
+                            worker.ReportProgress(0, new ProgressReport(Constants.ProgressMessage4, 0));
+                            break;
+                        }
 
-                    clientCfg.Write("StartUpdate", "UPDATE_END");
+                        Win32.WritePrivateProfileStringW("Version", "StartUpdate", "EXTRACT_END", clientCfg.Path);
+                        File.Delete(patch.Path);
 
-                    ++clientCfg.CurrentVersion;
-                    ++progressValue;
+                        worker.ReportProgress(0, new ProgressReport(Constants.ProgressMessage5, 0));
+                        Win32.WritePrivateProfileStringW("Version", "StartUpdate", "UPDATE_START", clientCfg.Path);
 
-                    var percentProgress = ((double)clientCfg.CurrentVersion / serverCfg.PatchFileVersion) * 100;
-                    if (percentProgress != 0)
-                        worker.ReportProgress((int)percentProgress, new ProgressReport(Constants.ProgressMessage7, 2));
+                        if (DataPatcher(worker) != 0)
+                        {
+                            worker.ReportProgress(0, new ProgressReport(Constants.ProgressMessage6, 0));
+                            break;
+                        }
 
-                    if (clientCfg.Write("CurrentVersion", clientCfg.CurrentVersion.ToString()) == 0)
-                    {
-                        worker.ReportProgress(0, new ProgressReport(Constants.ProgressMessage8, 0));
-                        break;
+                        Win32.WritePrivateProfileStringW("Version", "StartUpdate", "UPDATE_END", clientCfg.Path);
+
+                        ++clientCfg.CurrentVersion;
+                        ++progressValue;
+
+                        var percentProgress = ((double)clientCfg.CurrentVersion / serverCfg.PatchFileVersion) * 100;
+                        if (percentProgress != 0)
+                            worker.ReportProgress((int)percentProgress, new ProgressReport(Constants.ProgressMessage7, 2));
+
+                        var currentVersion = clientCfg.CurrentVersion.ToString();
+                        Win32.WritePrivateProfileStringW("Version", "CurrentVersion", currentVersion, clientCfg.Path);
                     }
                 }
+            }
+            catch (Exception ex)
+            {
+                var log = new Log();
+                log.Write(ex.ToString());
             }
         }
 
