ff7f64e1966c07f91e0bc4e16485446e1cd062af
kurtekat <142125482+kurtekat@users.noreply.github.com>
2024-10-20 15:40:08 -0400

    Create DataPatcherProgress.cs

---
+++ Updater/Core/DataPatcherProgress.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel;
+using Updater.Common;
+
+namespace Updater.Core
+{
+    public class DataPatcherProgress
+    {
+        private readonly BackgroundWorker _backgroundWorker = new();
+        public int Maximum { get; } = 0;
+        public int Value { get; set; } = 0;
+
+        public DataPatcherProgress(int fileCount, BackgroundWorker worker)
+        {
+            if (fileCount <= 0)
+                throw new ArgumentOutOfRangeException(nameof(fileCount));
+
+            Maximum = fileCount;
+            _backgroundWorker = worker;
+            _backgroundWorker.WorkerReportsProgress = true;
+        }
+
+        public void FilePatchedCallback()
+        {
+            Value++;
+            var percentProgress = (double)(Value / Maximum) * 100;
+            _backgroundWorker.ReportProgress((int)percentProgress, new ProgressReport(ByProgressBar: 1));
+        }
+    }
+}
