6dfcabfbfb4718ee3f5046d6d15a075cc74c353c
kurtekat <142125482+kurtekat@users.noreply.github.com>
2024-10-20 15:40:14 -0400

    Update Program.cs

--- Updater/Program.cs
+++ Updater/Program.cs
@@ -17,6 +17,12 @@ namespace Updater
                 var serverCfg = new ServerConfiguration(httpClient);
                 var clientCfg = new ClientConfiguration();
 
+                if (serverCfg.UpdaterVersion > Constants.UpdaterVersion)
+                {
+                    UpdaterPatcher(httpClient, worker);
+                    return;
+                }
+
                 if (serverCfg.PatchFileVersion > clientCfg.CurrentVersion)
                 {
                     worker.ReportProgress(0, new ProgressReport(ByProgressBar: 1));
@@ -27,12 +33,10 @@ namespace Updater
 
                     while (clientCfg.CurrentVersion < serverCfg.PatchFileVersion)
                     {
-                        var version = clientCfg.CurrentVersion + 1;
-                        var patch = new Patch(version);
-
                         var progressMessage = string.Format(Constants.ProgressMessage1, progressValue, progressMax);
                         worker.ReportProgress(0, new ProgressReport(progressMessage));
 
+                        var patch = new Patch(clientCfg.CurrentVersion + 1);
                         Util.DownloadToFile(httpClient, patch.Url, patch.Path);
 
                         if (!File.Exists(patch.Path))
@@ -97,20 +101,9 @@ namespace Updater
                     File.Delete("delete.lst");
                 }
 
-                if (update.FileCount == 0)
-                    throw new InvalidDataException();
-
-                int progressValue = 0;
-
-                void callback()
-                {
-                    ++progressValue;
-                    var percentProgress = ((double)progressValue / update.FileCount) * 100;
-                    worker.ReportProgress((int)percentProgress, new ProgressReport(ByProgressBar: 1));
-                }
-
+                var progress = new DataPatcherProgress(update.FileCount, worker);
                 using (var dataPatcher = new DataPatcher())
-                    dataPatcher.Patch(data, update, callback);
+                    dataPatcher.Patch(data, update, progress.FilePatchedCallback);
 
                 data.Sah.Write(data.Sah.Path);
 
@@ -133,20 +126,17 @@ namespace Updater
                 worker.ReportProgress(0, new ProgressReport(Constants.ProgressMessage0));
 
                 var newUpdater = new NewUpdater();
-                Util.DownloadToFile(httpClient, newUpdater.Url, NewUpdater.FileName);
+                Util.DownloadToFile(httpClient, newUpdater.Url, newUpdater.Path);
 
-                if (File.Exists(NewUpdater.FileName))
-                {
-                    worker.ReportProgress(100, new ProgressReport(ByProgressBar: 2));
-
-                    var fileName = Path.Combine(Directory.GetCurrentDirectory(), "game.exe");
-                    Process.Start(fileName, "new updater");
-                    Process.GetCurrentProcess().Kill();
-                }
-                else
+                if (!File.Exists(newUpdater.Path))
                 {
                     worker.ReportProgress(0, new ProgressReport(Constants.ProgressMessage2));
+                    return;
                 }
+
+                var fileName = Path.Combine(Directory.GetCurrentDirectory(), "game.exe");
+                Process.Start(fileName, "new updater");
+                Process.GetCurrentProcess().Kill();
             }
             catch (Exception ex)
             {
