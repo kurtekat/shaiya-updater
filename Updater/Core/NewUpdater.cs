95e464300cc554294af2f931cbd0f3710ecfbeef
kurtekat <142125482+kurtekat@users.noreply.github.com>
2024-10-20 15:39:57 -0400

    Update NewUpdaterTests.cs

--- Updater/Core/NewUpdater.cs
+++ Updater/Core/NewUpdater.cs
@@ -1,12 +1,16 @@
-﻿namespace Updater.Core
+﻿using System.IO;
+
+namespace Updater.Core
 {
     public class NewUpdater
     {
         public const string FileName = "new_updater.exe";
+        public readonly string Path = string.Empty;
         public readonly string Url = string.Empty;
 
         public NewUpdater()
         {
+            Path = System.IO.Path.Combine(Directory.GetCurrentDirectory(), FileName);
             Url = $"{Constants.Source}/shaiya/{FileName}";
         }
     }
