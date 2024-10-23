14502808004387eb1433dc0ec38847715172a760
kurtekat <142125482+kurtekat@users.noreply.github.com>
2024-10-23 15:02:03 -0400

    Update Kernel32.cs

--- Updater/Imports/Kernel32.cs
+++ Updater/Imports/Kernel32.cs
@@ -4,12 +4,18 @@ namespace Updater.Imports
 {
     static class Kernel32
     {
+        [DllImport("Kernel32", EntryPoint = "GetCurrentProcess")]
+        public static extern IntPtr GetCurrentProcess();
+
         [DllImport("Kernel32", EntryPoint = "GetPrivateProfileIntW", CharSet = CharSet.Unicode)]
         public static extern uint GetPrivateProfileIntW(string section, string key, int defaultValue, string fileName);
 
         [DllImport("Kernel32", EntryPoint = "GetPrivateProfileStringW", CharSet = CharSet.Unicode)]
         public static extern uint GetPrivateProfileStringW(string? section, string? key, string? defaultValue, char[] buffer, uint bufferSize, string fileName);
 
+        [DllImport("Kernel32", EntryPoint = "TerminateProcess")]
+        public static extern int TerminateProcess(IntPtr handle, int exitCode);
+        
         [DllImport("Kernel32", EntryPoint = "WritePrivateProfileStringW", CharSet = CharSet.Unicode)]
         public static extern int WritePrivateProfileStringW(string section, string? key, string? value, string fileName);
     }
