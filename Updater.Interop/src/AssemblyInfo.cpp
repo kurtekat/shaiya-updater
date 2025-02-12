1b67620f27ead8b73e6d15e057c0b9dfcdbe0c49
kurtekat <142125482+kurtekat@users.noreply.github.com>
2025-02-12 18:18:27 -0500

    Bump patch version

--- Updater.Interop/src/AssemblyInfo.cpp
+++ Updater.Interop/src/AssemblyInfo.cpp
@@ -12,5 +12,5 @@ using namespace System::Security::Permissions;
 [assembly:AssemblyCopyrightAttribute(L"Copyright (c) 2025 shaiya.dev")];
 [assembly:AssemblyTrademarkAttribute(L"")];
 [assembly:AssemblyCultureAttribute(L"")];
-[assembly:AssemblyVersionAttribute(L"1.0.2")];
+[assembly:AssemblyVersionAttribute(L"1.0.1")];
 [assembly:ComVisible(false)];
