ffec9351305b86f2a7691fb29472a7e751004f86
kurtekat <142125482+kurtekat@users.noreply.github.com>
2025-02-19 20:43:54 -0500

    Create MathHelperTests.cs

---
+++ Updater.Tests/Helpers/MathHelperTests.cs
@@ -0,0 +1,24 @@
+﻿using Updater.Helpers;
+
+namespace Updater.Tests.Helpers
+{
+    [TestFixture]
+    public class MathHelperTests
+    {
+        [Test]
+        public void ShouldCalculatePercentage()
+        {
+            for (int i = 1; i < 10; i++)
+            {
+                var percentage = MathHelper.CalculatePercentage(i, 10);
+                Assert.That(percentage, Is.EqualTo(i * 10));
+            }
+
+            for (int i = -10; i < 0; i++)
+            {
+                var percentage = MathHelper.CalculatePercentage(i, 10);
+                Assert.That(percentage, Is.EqualTo(i * 10));
+            }
+        }
+    }
+}
