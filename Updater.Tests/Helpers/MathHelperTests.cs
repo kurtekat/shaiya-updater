3d7a6ec9b5b2ebd6956c04309720167826b68c52
kurtekat <142125482+kurtekat@users.noreply.github.com>
2025-02-23 22:46:01 -0500

    Update MathHelperTests.cs

--- Updater.Tests/Helpers/MathHelperTests.cs
+++ Updater.Tests/Helpers/MathHelperTests.cs
@@ -5,20 +5,12 @@ namespace Updater.Tests.Helpers
     [TestFixture]
     public class MathHelperTests
     {
-        [Test]
-        public void ShouldCalculatePercentage()
+        [TestCase(-1, 1, ExpectedResult = -100)]
+        [TestCase(10, 1, ExpectedResult = 1000)]
+        [TestCase(1, 0, ExpectedResult = int.MinValue)]
+        public int CalculatePercentageTest(int part, int whole)
         {
-            for (int i = 1; i < 10; i++)
-            {
-                var percentage = MathHelper.CalculatePercentage(i, 10);
-                Assert.That(percentage, Is.EqualTo(i * 10));
-            }
-
-            for (int i = -10; i < 0; i++)
-            {
-                var percentage = MathHelper.CalculatePercentage(i, 10);
-                Assert.That(percentage, Is.EqualTo(i * 10));
-            }
+            return MathHelper.CalculatePercentage(part, whole);
         }
     }
 }
